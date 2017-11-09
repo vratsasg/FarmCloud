97ba848dbc2af72090465985bafd9e2961b4a5b4
dsmyrnaios <dsmyrnaios@gmail.com>
2017-11-09 12:23:37 +0200

    some changes on userprofile jpa repository

--- src/main/java/com/webstart/repository/UserProfileJpaRepository.java
+++ src/main/java/com/webstart/repository/UserProfileJpaRepository.java
@@ -1,16 +1,9 @@
 package com.webstart.repository;
 
 import com.webstart.model.UserProfile;
-import com.webstart.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-
 
 public interface UserProfileJpaRepository extends JpaRepository<UserProfile,Integer> {
-    //UserProfile findByUser_id(int id);
 
 //    @Query("select new com.webstart.model.UserProfile(up.userId, up.firstname, up.address, up.fathersname, up.lastname, up.addressnum, up.zipcode, up.telephone, up.mobile, up.dateofbirth, null) " +
 //            "from UserProfile as up " +
